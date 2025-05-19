781b0221f284d8cd53b479ac9dedb95c3112494e
Satyampandey2645 <104256013+Satyampandey2645@users.noreply.github.com>
2025-05-19 18:40:26 +0530

    Update settings.py

--- national_bazar_compare/settings.py
+++ national_bazar_compare/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
